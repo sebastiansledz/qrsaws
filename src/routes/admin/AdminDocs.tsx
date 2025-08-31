89e8d019b8fda917a33d31e46f096be66c46c7b7
sebastiansledz <sebastian.sledz@gmail.com>
2025-08-31 18:02:08 +0200

    Updated AdminDocs.tsx

--- src/routes/admin/AdminDocs.tsx
+++ src/routes/admin/AdminDocs.tsx
@@ -145,7 +145,7 @@ export default function AdminDocs() {
       <div className="bg-transparent">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="py-6">
-            <h1 className="text-3xl font-bold tracking-tight">
+            <h1 className="text-3xl font-bold tracking-tight>
               Panel Administratora
             </h1>
             <p className="mt-2 text-gray-600">
