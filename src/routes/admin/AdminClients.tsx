8652e4feab62c9597c4b513c09d2381c6d484160
sebastiansledz <sebastian.sledz@gmail.com>
2025-08-31 12:33:36 +0200

    Updated AdminClients.tsx

--- src/routes/admin/AdminClients.tsx
+++ src/routes/admin/AdminClients.tsx
@@ -148,6 +148,7 @@ export const AdminClients: React.FC = () => {
   };
 
   return (
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 space-y-6">  
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 space-y-6">
         <motion.div
@@ -291,6 +292,7 @@ export const AdminClients: React.FC = () => {
           )}
         </div>
       </Overlay>
+    </div>
   );
 };
 
