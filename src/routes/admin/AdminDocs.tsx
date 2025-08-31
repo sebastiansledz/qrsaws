c8d17853af5d56830607192a50596baddbecc9e0
sebastiansledz <sebastian.sledz@gmail.com>
2025-08-31 19:54:29 +0200

    Updated AdminDocs.tsx

--- src/routes/admin/AdminDocs.tsx
+++ src/routes/admin/AdminDocs.tsx
@@ -164,7 +164,7 @@ export default function AdminDocs() {
             <Plus className="h-4 w-4" />
             Dodaj piłę
           </Button>
-              )}
+              
             </div>
           </div>
 
